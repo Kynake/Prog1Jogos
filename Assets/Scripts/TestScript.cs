16dd214675cb48699ce9e2540d45b010eda5b109
Pedro Ortiz Bledow <kynakerisk@gmail.com>
2020-10-20 22:49:30 -0300

    Create new TestScript

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour {
+    public float Speed;
+
+    public Transform obj;
+
+    void Start() {
+
+    }
+
+    void Update() {
+        Vector3 pos = transform.position;
+        
+    }
+}
